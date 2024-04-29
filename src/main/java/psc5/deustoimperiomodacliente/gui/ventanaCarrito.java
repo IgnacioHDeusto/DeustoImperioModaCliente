16db56b2015d713f9de8257d8d8e606784cb6ff7
Etxaaniiz <aimar.etxaniz@opendeusto.es>
2024-04-29 21:51:36 +0200

    Mejora de ventanaCarrito (1)

--- src/main/java/psc5/deustoimperiomodacliente/gui/ventanaCarrito.java
+++ src/main/java/psc5/deustoimperiomodacliente/gui/ventanaCarrito.java
@@ -2,18 +2,27 @@ package psc5.deustoimperiomodacliente.gui;
 
 import psc5.deustoimperiomodacliente.post.Articulo;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 public class ventanaCarrito extends JFrame{
     
     private List<Articulo> productosCarrito;
     private JTable tablaCarrito;
+    private JButton pagarButton = new JButton("Finalizar y Pagar");
+    JButton eliminarButton = new JButton("Eliminar");
+    JPanel buttonPanel = new JPanel();
+    
+    
 
     public ventanaCarrito(List<Articulo> productosCarrito) {
         this.productosCarrito = productosCarrito;
@@ -46,8 +55,25 @@ public class ventanaCarrito extends JFrame{
             modeloTabla.addRow(fila);
         }
 
+        // Añadir un ActionListener al botón "Eliminar"
+        eliminarButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        // Obtener la fila seleccionada
+        int selectedRow = tablaCarrito.getSelectedRow();
+
+        // Si hay una fila seleccionada, eliminarla
+        if (selectedRow != -1) {
+            modeloTabla.removeRow(selectedRow);
+        }
+    }
+        });
+
         // Agregar la tabla a la ventana
+        buttonPanel.add(eliminarButton);
+        buttonPanel.add(pagarButton);
         getContentPane().add(tablaCarrito);
         getContentPane().add(new JScrollPane(tablaCarrito), BorderLayout.CENTER);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
     }
 }
